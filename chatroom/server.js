4095a22a7a0cd74e5a0383b845f05377765b5d64
Ajoy Mahato <ajoymahato16@gmail.com>
2023-02-17 21:05:23 +0530

    databse connection and save chat to data base done

--- chatroom/server.js
+++ chatroom/server.js
@@ -1,8 +1,14 @@
+const mongoose = require("mongoose");
+const Msg = require('./models/messages');
 const express = require("express");
 const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const mongoDB = 'mongodb+srv://ajoymahato16:PJ6yH8PzKMR3igsI@cluster0.wmldu9m.mongodb.net/chat?retryWrites=true&w=majority';
+mongoose.connect(mongoDB).then(()=>{
+    console.log("Test song.");
+}).catch(err =>console.log(err));
 
 app.use(express.static(path.join(__dirname+"/public")));
 io.on("connection", function(socket){
@@ -13,7 +19,11 @@ io.on("connection", function(socket){
         socket.broadcast.emit("update",username+ " has left the conversation");
     })
     socket.on("chat",function(message){
-        socket.broadcast.emit("chat",message);
+        const chatmessage = new Msg({msg:message});
+        chatmessage.save().then(()=>{
+            socket.broadcast.emit("chat",message);
+        })
+        
     })
 })
 server.listen(5000);
